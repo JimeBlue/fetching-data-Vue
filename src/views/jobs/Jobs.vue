6d3eb0383ff527f08d3627dbe8786499f310471b
Jimena Miguez <jimeblau@gmail.com>
2022-08-28 16:49:44 +0200

    delete comments

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -8,34 +8,6 @@
     </div>
   </div>
 </template>
-<!-- EXPLANATION: instead of using the data from our
-components, we´re going to fetch data from an API.
-We´re going to use a service called json server,
-which simulates an API to which we can make requests to.
-Json server allows us to use a json file containing data
-as a database resource with various different endpoints.
- -->
-<!-- 1) In the root of the project, create a data folder,
-inside it create the db.json file. In this file, include 
-the data you want in json format. Note: in json files, any
-top level keys are called resources. In our case, jobs
-is a resource. -->
-<!-- 2) Install the json server package locally inside this project: 
-npm install json-server -->
-<!-- 3) To watch our db.json file, we run 
-  npx json-server --watch + the path of our json file: data/db.json.
-So, run  npx json-server --watch data/db.json -->
-<!-- 4) After running the watch command, we´ll get 
-a local port with the end point /jobs to get the jobs
-resource we have in the db.json file. So, when we open
-the local port in a browser, it´s going to send a get
-request for the jobs resouce and it´s going to send 
-back json with all the different objects included in 
-the jobs resource-->
-<!-- IMPORTANT: if you want to see how to get and post data
-in a real database, not an API, go to repo 
-vuejs-playlist-vue2-basics-continue-CLI, branches: 
-posting to firebase and retrieving blogs from firebase-->
 <script>
 export default {
   data() {
